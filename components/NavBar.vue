ccc3545b234be37e769b8ffa84c21c21a25f49a1
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-06-15 20:09:20 +0100

    feature: Navbar component

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,46 @@
 <script setup>
 
+  import { useDark, useToggle } from '@vueuse/core';
+
+  const isDark = useDark();
+
+  const toggleDark = useToggle(isDark);
+
+
+
 </script>
 
 <template>
-    <nav>
-      <h1 class="text-teal-700 text-3xl">Welcome</h1>
+    <nav class="max-w-[1100px] flex justify-between items-center mx-auto p-2">
+      <div class="flex items-center">
+        <img src="../assets/images/logo.png" alt="Website Logo" class="w-12">
+        <h1 class="text-2xl font-bold ">Lesson Teachers Hub</h1>
+      </div>
+      <ul class="hidden md:flex justify-between space-x-4 text-xl font-semibold">
+        <li>
+            <NuxtLink to="#" class="hover:text-2xl">Home</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="#" class="hover:text-2xl">About</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="#" class="hover:text-2xl">Contact Us</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="#" class="hover:text-2xl">Login</NuxtLink>
+        </li>
+      </ul>
+      <div class="flex justify-between">
+        <button class="hidden sm:inline bg-violet-500 text-white text-lg font-semibold py-2 px-3 rounded-2xl mr-3">
+            <NuxtLink>Sign Up</NuxtLink>
+        </button>
+        <span v-if="isDark" @click="toggleDark()">
+          <img src="../assets/images/light-mode.png" alt="Light Mode Switch" class="w-8">
+        </span>
+        <span v-if="!isDark" @click="toggleDark()">
+          <img src="../assets/images/dark-mode.png" alt="Light Mode Switch" class="w-6">
+        </span>
+         </div>
+      
     </nav>
 </template>
