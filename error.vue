0e638f5450d77c0e8e99ac0ad6f618ec9674529c
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-06-15 20:40:01 +0100

    error page

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <main class="w-screen h-screen">
+        <div class="mx-auto p-4">
+            <h2 class="text-7xl text-center text-slate-800 dark:text-slate-200">Oops......</h2>
+            <p class="text-xl text-center text-slate-800 dark:text-slate-200">Seems like you took a wrong turn there!</p>
+            <button class="text-white px-3 py-2 mx-auto">Go Home</button>
+        </div>
+    </main>
+</template>
