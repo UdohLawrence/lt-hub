d8d603ffb26c1f082deeae1763b1bdc16b297c9d
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-06-20 20:01:32 +0100

    final error page

--- error.vue
+++ error.vue
@@ -1,13 +1,19 @@
 <script setup>
+ defineProps(['error']); 
 
+ const handleClearError = () => {
+    clearError({redirect: '/'});
+ }
 </script>
 
 <template>
-    <main class="w-screen h-screen">
-        <div class="mx-auto p-4">
-            <h2 class="text-7xl text-center text-slate-800 dark:text-slate-200">Oops......</h2>
-            <p class="text-xl text-center text-slate-800 dark:text-slate-200">Seems like you took a wrong turn there!</p>
-            <button class="text-white px-3 py-2 mx-auto">Go Home</button>
+    <main class="w-screen h-screen flex items-center justify-center">
+        <div class="mx-auto p-4 shadow-2xl bg-white dark:bg-slate-600">
+            <h2 class="text-6xl text-center font-bold">{{ error.statusCodes }}</h2>
+            <p class="text-7xl text-center text-slate-800 my-5 dark:text-slate-200">Oops......</p>
+            <p class="text-xl text-center text-slate-800 dark:text-slate-200 my-5">Seems like you took a wrong turn there!</p>
+            <p class="text-7xl text-center text-slate-800 my-5 dark:text-slate-200">{{ error.message }}</p>
+            <button class="text-white bg-violet-500 rounded-xl px-3 py-2 mx-auto" @click="handleClearError">Go Home</button>
         </div>
     </main>
 </template>
