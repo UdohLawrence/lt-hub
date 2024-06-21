1cd6cb43ff4340f3579e459119c3f1a8c036646b
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-06-21 09:22:50 +0100

    fixed: meta tags for the head element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,9 @@ export default defineNuxtConfig({
     head: {
       title: 'Lesson Teachers Hub',
       meta: [
-        {name: 'description', content: 'Get Professional Home Tutors for your treasured child, anywhere in Nigeria.'}
+        {name: 'description', content: 'Get Professional Home Tutors for your treasured child, anywhere in Nigeria.'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
+        {charset: 'utf-8'}
       ],
       link: [
         {rel: 'icon', href: './assets/images/favico.ico'}
